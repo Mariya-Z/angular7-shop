da2ec0f42a0952211d75f2345db71229386ee6a6
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-04-08 17:34:39 +0300

    add listener for mouseleave

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,20 +1,24 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
   @HostBinding('style.background') private background;
 
-  @HostListener('click') public onClick(event) {
+  @HostListener('click') public onClick() {
     this.background = 'aqua';
   }
 
-  @HostListener('mouseenter') public onMouseEnter(event) {
+  @HostListener('mouseenter') public onMouseEnter() {
     this.background = 'red';
+  }
+
+  @HostListener('mouseleave') public onMouseLeave() {
+    this.background = this.el.nativeElement.background;
   }
 
 }
