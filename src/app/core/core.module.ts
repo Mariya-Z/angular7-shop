a014bc9db739cd50ede7970ff406b1805a8dc3e5
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-04-16 11:43:14 +0300

    add directive

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { CoreServicesModule } from './core-services.module';
+import { ShowUpDirective } from './directive/show-up.directive';
 
 @NgModule({
-  declarations: [ContactUsComponent],
+  declarations: [ContactUsComponent, ShowUpDirective],
   exports: [ContactUsComponent],
   imports: [CommonModule, CoreServicesModule],
 })
