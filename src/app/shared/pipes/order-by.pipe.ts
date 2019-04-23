a4d0235fa00ba1481f51f98ce9d26c0b55d2221f
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-04-23 19:10:56 +0300

    add forms module

--- src/app/shared/pipes/order-by.pipe.ts
+++ src/app/shared/pipes/order-by.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'orderBy',
-  pure: false,
+  // pure: false,
 })
 export class OrderByPipe implements PipeTransform {
   transform(products: any, field: string, flag: boolean, args?: any): any {
