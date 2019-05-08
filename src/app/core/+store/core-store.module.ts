3409ac357c200371209cde2caf1ccfe1fe9e138e
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-05-08 17:06:33 +0300

    add ngrx store devtools

--- src/app/core/+store/core-store.module.ts
+++ src/app/core/+store/core-store.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from './../../../environments/environment';
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     StoreModule.forRoot({}),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ]
 })
 export class CoreStoreModule { }
