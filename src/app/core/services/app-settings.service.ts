426401e39b70335c178d9f4bb72bdab56af3258a
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-05-10 11:06:50 +0300

    add AppSettingsService

--- src/app/core/services/app-settings.service.ts
+++ src/app/core/services/app-settings.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { CoreServicesModule } from '../core-services.module';
 import { LocalStorageService } from './local-storage.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: CoreServicesModule,
@@ -9,5 +10,34 @@ import { LocalStorageService } from './local-storage.service';
 export class AppSettingsService {
   settings = [];
 
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(
+    private localStorageService: LocalStorageService,
+    private http: HttpClient,
+  ) {}
+
+  loadSettings() {
+    const req = this.localStorageService.getSettings('appSettings');
+    if (req) {
+      this.settings = req;
+    } else {
+      this.getJSON();
+      this.localStorageService.saveSettings('appSettings', this.settings);
+    }
+  }
+
+  private getJSON() {
+    return this.http.get('.assets/app-settings.json').subscribe(
+      res => {
+        this.localStorageService.saveSettings('appSettings', res['requests']);
+      },
+      () => this.getDefaultSettings(),
+    );
+  }
+
+  private getDefaultSettings() {
+    console.log(
+      'Default settins has been used, app-settings.json was not found',
+    );
+    this.localStorageService.saveSettings('appSettings', this.settings);
+  }
 }
