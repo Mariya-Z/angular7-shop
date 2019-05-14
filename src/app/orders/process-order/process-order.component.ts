5b433b2654b6ac5e1231c10426ca22262c172995
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-05-14 13:48:10 +0300

    add Validators

--- src/app/orders/process-order/process-order.component.ts
+++ src/app/orders/process-order/process-order.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { Order } from '../model/order';
 
 @Component({
@@ -11,10 +16,39 @@ export class ProcessOrderComponent implements OnInit {
   order: Order = new Order();
   orderForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-   this.formInit()
+    // this.formInit();
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.orderForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      lastname: [
+        { value: 'Zhyrytskyy', disabled: false },
+        [Validators.required, Validators.maxLength(30)],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'),
+          Validators.email,
+        ],
+      ],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.pattern('[+]?[0-9]+'),
+        ],
+      ],
+      delivery: true,
+      adress: ['', [Validators.minLength(5), Validators.maxLength(50)]],
+    });
   }
 
   private formInit() {
@@ -28,7 +62,26 @@ export class ProcessOrderComponent implements OnInit {
     });
   }
 
+  onSetDelivery(delivery: boolean) {
+    const adressControl = this.orderForm.get('adress');
+    if (delivery) {
+      adressControl.clearValidators();
+    } else {
+      adressControl.setValidators([
+        Validators.minLength(5),
+        Validators.maxLength(50),
+      ]);
+    }
+    adressControl.updateValueAndValidity();
+  }
+
   onSave() {
     console.log('order');
+    // Form model
+    console.log(this.orderForm);
+    // Form value w/o disabled controls
+    console.log(`Saved: ${JSON.stringify(this.orderForm.value)}`);
+    // Form value w/ disabled controls
+    console.log(`Saved: ${JSON.stringify(this.orderForm.getRawValue())}`);
   }
 }
