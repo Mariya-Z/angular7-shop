acadfbbef6fc93696fc7c5b24226c5cfed215362
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-05-14 18:03:33 +0300

    add watchValueChanges

--- src/app/orders/process-order/process-order.component.ts
+++ src/app/orders/process-order/process-order.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -7,62 +7,79 @@ import {
 } from '@angular/forms';
 import { Order } from '../model/order';
 
+// rxjs
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-process-order',
   templateUrl: './process-order.component.html',
   styleUrls: ['./process-order.component.scss'],
 })
-export class ProcessOrderComponent implements OnInit {
+export class ProcessOrderComponent implements OnInit, OnDestroy {
   order: Order = new Order();
   orderForm: FormGroup;
+  private sub: Subscription;
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     // this.formInit();
     this.buildForm();
+    this.watchValueChanges();
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
   private buildForm() {
-    this.orderForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(2)]],
-      lastname: [
-        { value: 'Zhyrytskyy', disabled: false },
-        [Validators.required, Validators.maxLength(30)],
-      ],
-      email: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'),
-          Validators.email,
+    this.orderForm = this.fb.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(2)]],
+        lastname: [
+          { value: 'Zhyrytskyy', disabled: false },
+          [Validators.required, Validators.maxLength(30)],
         ],
-      ],
-      phone: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(5),
-          Validators.pattern('[+]?[0-9]+'),
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'),
+            Validators.email,
+          ],
         ],
-      ],
-      delivery: true,
-      adress: ['', [Validators.minLength(5), Validators.maxLength(50)]],
-    });
+        phone: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.pattern('[+]?[0-9]+'),
+          ],
+        ],
+        delivery: true,
+        adress: ['', [Validators.minLength(5), Validators.maxLength(50)]],
+      },
+      { updateOn: 'blur' },
+    );
   }
 
-  private formInit() {
-    this.orderForm = new FormGroup({
-      name: new FormControl(),
-      lastname: new FormControl(),
-      email: new FormControl(),
-      phone: new FormControl(),
-      delivery: new FormControl(),
-      adress: new FormControl(),
-    });
+  onSave() {
+    console.log('order');
+    // Form model
+    console.log(this.orderForm);
+    // Form value w/o disabled controls
+    console.log(`Saved: ${JSON.stringify(this.orderForm.value)}`);
+    // Form value w/ disabled controls
+    console.log(`Saved: ${JSON.stringify(this.orderForm.getRawValue())}`);
   }
 
-  onSetDelivery(delivery: boolean) {
+  private watchValueChanges() {
+    this.sub = this.orderForm
+      .get('delivery')
+      .valueChanges.subscribe(value => this.setDelivery(value));
+  }
+
+  private setDelivery(delivery: boolean) {
     const adressControl = this.orderForm.get('adress');
     if (delivery) {
       adressControl.clearValidators();
@@ -75,13 +92,14 @@ export class ProcessOrderComponent implements OnInit {
     adressControl.updateValueAndValidity();
   }
 
-  onSave() {
-    console.log('order');
-    // Form model
-    console.log(this.orderForm);
-    // Form value w/o disabled controls
-    console.log(`Saved: ${JSON.stringify(this.orderForm.value)}`);
-    // Form value w/ disabled controls
-    console.log(`Saved: ${JSON.stringify(this.orderForm.getRawValue())}`);
+  private formInit() {
+    this.orderForm = new FormGroup({
+      name: new FormControl(),
+      lastname: new FormControl(),
+      email: new FormControl(),
+      phone: new FormControl(),
+      delivery: new FormControl(),
+      adress: new FormControl(),
+    });
   }
 }
