c57cb8d8ca2c059aad17c1b44f8797a135060cee
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-05-15 14:18:18 +0300

    move validation in class

--- src/app/orders/process-order/process-order.component.ts
+++ src/app/orders/process-order/process-order.component.ts
@@ -4,12 +4,13 @@ import {
   FormGroup,
   FormControl,
   Validators,
+  AbstractControl,
 } from '@angular/forms';
 import { Order } from '../model/order';
 
 // rxjs
 import { Subscription } from 'rxjs';
-
+import { debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'app-process-order',
   templateUrl: './process-order.component.html',
@@ -18,7 +19,40 @@ import { Subscription } from 'rxjs';
 export class ProcessOrderComponent implements OnInit, OnDestroy {
   order: Order = new Order();
   orderForm: FormGroup;
+  validationMessage = {
+    name: '',
+    lastname: '',
+    email: '',
+    phone: '',
+    adress: '',
+  };
   private sub: Subscription;
+
+  private validationMessagesMap = {
+    name: {
+      required: 'Please enter your name.',
+      minlength: 'The first name must be longer than 2 characters.',
+    },
+    lastname: {
+      required: 'Please enter your last name.',
+      maxlength: 'The last name must be shorter than 30 characters.',
+    },
+    email: {
+      required: 'Please enter your email address.',
+      pattern: 'Please enter a incorrect email address.',
+      email: 'Please enter a valid email address.',
+    },
+    phone: {
+      required: 'Please enter your phone.',
+      pattern: 'Please use only numbers and +.',
+      minlength: 'The phone must be longer than 5 characters.',
+    },
+    adress: {
+      required: 'Please enter your address.',
+      minlength: 'The adress must be longer than 5 characters.',
+      maxlength: 'The adress must be shorter than 50 characters.',
+    },
+  };
 
   constructor(private fb: FormBuilder) {}
 
@@ -32,12 +66,38 @@ export class ProcessOrderComponent implements OnInit, OnDestroy {
     this.sub.unsubscribe();
   }
 
+  onSave() {
+    console.log('order');
+    // Form model
+    console.log(this.orderForm);
+    // Form value w/o disabled controls
+    console.log(`Saved: ${JSON.stringify(this.orderForm.value)}`);
+    // Form value w/ disabled controls
+    console.log(`Saved: ${JSON.stringify(this.orderForm.getRawValue())}`);
+  }
+
+  onBlur(name: string) {
+    const emailControl = this.orderForm.get(name);
+    this.setValidationMessage(emailControl, name);
+  }
+
+  private setValidationMessage(c: AbstractControl, controlName: string) {
+    console.log(this.validationMessage[controlName]);
+    this.validationMessage[controlName] = '';
+
+    if ((c.touched || c.dirty) && c.errors) {
+      this.validationMessage[controlName] = Object.keys(c.errors)
+        .map(key => this.validationMessagesMap[controlName][key])
+        .join(' ');
+    }
+  }
+
   private buildForm() {
     this.orderForm = this.fb.group(
       {
         name: ['', [Validators.required, Validators.minLength(2)]],
         lastname: [
-          { value: 'Zhyrytskyy', disabled: false },
+          { value: '', disabled: false },
           [Validators.required, Validators.maxLength(30)],
         ],
         email: [
@@ -66,24 +126,52 @@ export class ProcessOrderComponent implements OnInit, OnDestroy {
           ],
         ],
       },
-      { updateOn: 'blur' },
+      // { updateOn: 'blur' },
     );
-  }
-
-  onSave() {
-    console.log('order');
-    // Form model
-    console.log(this.orderForm);
-    // Form value w/o disabled controls
-    console.log(`Saved: ${JSON.stringify(this.orderForm.value)}`);
-    // Form value w/ disabled controls
-    console.log(`Saved: ${JSON.stringify(this.orderForm.getRawValue())}`);
   }
 
   private watchValueChanges() {
     this.sub = this.orderForm
       .get('delivery')
       .valueChanges.subscribe(value => this.setDelivery(value));
+
+    const nameControl = this.orderForm.get('name');
+    const subName = nameControl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(value => {
+        this.setValidationMessage(nameControl, 'name');
+        console.log(this.validationMessage);
+      });
+
+    const lastnameControl = this.orderForm.get('lastname');
+    const subLastame = lastnameControl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(value => {
+        this.setValidationMessage(lastnameControl, 'lastname');
+        console.log(this.validationMessage);
+      });
+
+    const emailControl = this.orderForm.get('email');
+    const subEmail = emailControl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(value => this.setValidationMessage(emailControl, 'email'));
+
+    const phoneControl = this.orderForm.get('phone');
+    const subPhone = phoneControl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(value => this.setValidationMessage(phoneControl, 'phone'));
+
+    const adressControl = this.orderForm.get('adress');
+    const subAdress = adressControl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(value => this.setValidationMessage(adressControl, 'adress'));
+
+    this.sub
+      .add(subName)
+      .add(subLastame)
+      .add(subEmail)
+      .add(subPhone)
+      .add(subAdress);
   }
 
   private setDelivery(delivery: boolean) {
